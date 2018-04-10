6e1fb792e69ea8382175d0053ebe814f3da64d4d
Pierre Souquet <psouquet@linagora.com>
2018-04-10 10:24:35 +0200

    feature: predefined user added before run
    First admin user is added to DB before run, can be passed as argument
    
    Related to #7

--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-# Place holder script to configure cockpit environment
+# Migrate DB
+java -jar lib/petals-cockpit-*-capsule.jar db migrate conf/config.yml
+
+# Get user from default and/or arguments and add it to DB
+cp_user="admin"
+cp_name="admin"
+cp_pass="admin"
+
+[ ! -z "${COCKPIT_USER+x}" ] && cp_user="$COCKPIT_USER"
+[ ! -z "${COCKPIT_USERNAME+x}" ] && cp_name="$COCKPIT_USERNAME"
+[ ! -z "${COCKPIT_PASS+x}" ] && cp_pass="$COCKPIT_PASS"
+
+java -jar lib/petals-cockpit-*-capsule.jar add-user -u $cp_user -n $cp_name -p $cp_pass -a conf/config.yml
 
 # Launch Petals-cockpit
 /opt/petals-cockpit/petals-cockpit.sh
