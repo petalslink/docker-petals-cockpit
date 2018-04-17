1ddabc2b19b599606022bec5d0489c84b5cf6f64
Pierre Souquet <psouquet@linagora.com>
2018-04-17 11:23:48 +0200

    generate workspace at run

--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -12,7 +12,12 @@ cp_pass="admin"
 [ ! -z "${COCKPIT_USERNAME+x}" ] && cp_name="$COCKPIT_USERNAME"
 [ ! -z "${COCKPIT_PASS+x}" ] && cp_pass="$COCKPIT_PASS"
 
-java -jar lib/petals-cockpit-*-capsule.jar add-user -u $cp_user -n $cp_name -p $cp_pass -a conf/config.yml
+if [ ! -z "${COCKPIT_WORKSPACE+x}" ]
+then
+  java -jar lib/petals-cockpit-*-capsule.jar add-user -u $cp_user -n $cp_name -p $cp_pass -w $COCKPIT_WORKSPACE -a conf/config.yml
+else 
+  java -jar lib/petals-cockpit-*-capsule.jar add-user -u $cp_user -n $cp_name -p $cp_pass -a conf/config.yml
+fi
 
 # Launch Petals-cockpit
 /opt/petals-cockpit/petals-cockpit.sh
